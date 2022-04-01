6ad1b5174b31db99cbbc4517abc98bc5a4f6a5ef
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-01 16:16:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hackgit12/evamari.git /EvaMaria
+  git clone https://github.com/hackgit12/AEFX.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
